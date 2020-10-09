adfb0e52cf224e0f8c7a35365b2bbb8c53844086
Björn Kopiske <b.kopiske@iplabs.de>
2020-10-09 15:11:05 +0200

    Enable databinding

--- app/src/main/java/com/example/databindingapp/FirstFragment.kt
+++ app/src/main/java/com/example/databindingapp/FirstFragment.kt
@@ -4,25 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.databindingapp.databinding.FragmentFirstBinding
 
 class FirstFragment : Fragment() {
 
     private val viewModel: FirstViewModel by viewModels()
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView = view.findViewById<TextView>(R.id.timestamp)
-        textView.text = resources.getString(R.string.timestamp, viewModel.getDate())
+        binding.timestamp.text = resources.getString(R.string.timestamp, viewModel.getDate())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
