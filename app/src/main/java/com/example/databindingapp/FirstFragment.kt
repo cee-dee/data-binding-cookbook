37b1ee3db71d886a8d2163160c7274631e257987
Björn Kopiske <b.kopiske@iplabs.de>
2020-10-09 15:04:09 +0200

    Add first sample code for demonstrating data binding usages

---
+++ app/src/main/java/com/example/databindingapp/FirstFragment.kt
@@ -0,0 +1,28 @@
+package com.example.databindingapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+
+class FirstFragment : Fragment() {
+
+    private val viewModel: FirstViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val textView = view.findViewById<TextView>(R.id.timestamp)
+        textView.text = resources.getString(R.string.timestamp, viewModel.getDate())
+    }
+
+}
